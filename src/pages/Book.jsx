56c75ed8bdb0a47b45f288368d8acca3809fb03d
Sojung <sojunglyy@gmail.com>
2025-12-10 15:36:13 +0100

    style a single book info page

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { books } from "../assets/books";
 
 const Book = () => {
-  return <div>Book</div>;
+  const { bookId } = useParams();
+  const [currentBook, setCurrentBook] = useState(null);
+
+  const fetchBookData = useCallback(() => {
+    books.find((book) => {
+      if (book.id === bookId) {
+        setCurrentBook(book);
+      }
+    });
+  }, [bookId]);
+
+  useEffect(() => {
+    fetchBookData();
+  }, [fetchBookData]);
+
+  // checks if the book data exists before rendering
+  if (!currentBook) {
+    return (
+      <section>
+        <p>Book not found</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <div className="flex flex-col md:flex-row gap-10 my-10 xl:mb-20">
+        <div className="book-img w-full flex justify-center md:max-w-[50%]">
+          <img
+            src={currentBook.images}
+            alt="book cover image"
+            className="w-full md:w-4/5 lg:w-2/3 xl:w-1/2"
+          />
+        </div>
+        <div className="book-info">
+          <p className="text-xl">{currentBook.title}</p>
+          <p>by {currentBook.author}</p>
+          <hr className="my-10 text-gray-500" />
+          <p>{currentBook.description}</p>
+          <p className="my-5">category: {currentBook.category}</p>
+          <p>€{currentBook.price}</p>
+          <button
+            type="button"
+            className="my-10 border rounded-md p-2 cursor-pointer hover:bg-black hover:text-white"
+          >
+            Add To Cart
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Book;
